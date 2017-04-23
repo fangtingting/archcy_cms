dd7c0e6f0b8e26d224586b83c897aa79f060091c
ftt <1298022139@qq.com>
2017-04-23 16:32:44 +0800

    smartadmin搭建

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :articles 
+  root 'dashboard#index'
 
-  match 'files_list' => 'articles#files_list',via: [:get,:post,:delete]
-    
-  post 'ckeditor_upload_pic', to: 'ckeditor/pictures#upload_pic'
-
+  match 'files_list' => 'articles#files_list',via: [:get,:post,:delete]    
 end
 
 # 加载某个路径下的路由
